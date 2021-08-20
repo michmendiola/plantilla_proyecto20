03cdcc0987b382096d459c923e4243622d657ea9
michmendiola <86401865+michmendiola@users.noreply.github.com>
2021-08-19 23:39:47 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,31 @@
+var canvas,bg;
+var together;
+var tom, tomImg1,tomImg2;
+var jerry, jerryImg1,jerryImg2;
+
+function preload() {
+    
+
+}
+
+function setup(){
+    canvas = createCanvas(1000,800);
+
+    
+
+}
+
+function draw() {
+
+    background(bg);
+
+    
+
+    drawSprites();
+}
+
+
+function keyPressed(){
+
+    
+}
